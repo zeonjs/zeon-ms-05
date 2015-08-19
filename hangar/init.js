f7e828665b2fb07df67ec6747006037720488885
Ronge <wrj898@gmail.com>
2015-08-19 12:55:35 +0800

    Add auto generate folders

--- hangar/init.js
+++ hangar/init.js
@@ -3,6 +3,7 @@
 var chalk = require('chalk');
 var path = require('path');
 var fs = require('fs-extra');
+var Yaml = require("yamljs");
 
 var assetDir = path.join(__dirname, '../assets');
 
@@ -12,6 +13,14 @@ exports = module.exports = function (args) {
 
   fs.copy(assetDir, baseDir, function (err) {
     if (err) return console.error(err)
+	// 遍历生成目录
+	var nativeObject = Yaml.load("./zeon-config.yml");
+	var dirs = nativeObject.dir;
+	for(var value in dirs){
+		fs.emptyDir(dirs[value],function(err){
+			if (err) console.log('# ' + chalk.red(err));
+		});
+	}
     console.log('# ' + chalk.green('MS-05 Zaku') + ' is init.');
   });
 
